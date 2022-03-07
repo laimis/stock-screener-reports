a465a4c776b52de2dd6ab242c4d6c6c95eb8c8ab
unknown <laimis@gmail.com>
2022-03-07 11:42:28 -0800

    add types and generate the list

--- Program.fs
+++ Program.fs
@@ -1,8 +1,7 @@
-﻿open HtmlAgilityPack
+﻿
+open HtmlAgilityPack
 open System
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
-
+open FinvizScraper
 
 let url = "https://finviz.com/screener.ashx?v=111&s=ta_newhigh&f=fa_salesqoq_high,sh_avgvol_o200,sh_opt_optionshort,sh_price_o10,sh_relvol_o1.5,ta_perf_dup&ft=4&o=-volume"
 
@@ -18,15 +17,49 @@ let nodes = doc.DocumentNode.SelectNodes("//table[@id='screener-views-table']/tr
 
 // 3rd node has the tickers
 
-nodes 
-    |> Seq.skip 3 // skip three tr nodes
-    |> Seq.take 1 // take one that has the tickers table
-    |> Seq.collect (fun n -> n.ChildNodes) // this should contain #text, <td>, #text
-    |> Seq.skip 1 // skip one #text
-    |> Seq.take 1 // take one that has td
-    |> Seq.collect (fun n -> n.ChildNodes) // this should contain #text <table> #text
-    |> Seq.skip 1 // skip one #text
-    |> Seq.take 1 // take table
-    |> Seq.collect (fun n -> n.ChildNodes)
-    |> Seq.skip 1 // skip header row
-    |> Seq.iter (fun n -> Console.WriteLine(n.Name))
+let skipAndTake skip take seq =
+    seq 
+    |> Seq.skip skip
+    |> Seq.take take
+
+let extractValueFromScreenerCell (node:HtmlNode) =
+    let value = node.ChildNodes[0].InnerText
+    // printf "%s\n" value
+    value
+
+let processScreenerRow (node:HtmlNode) =
+    
+    let tickerNode = extractValueFromScreenerCell node.ChildNodes[2]
+    let companyNode = extractValueFromScreenerCell node.ChildNodes[3]
+    let sectorNode = extractValueFromScreenerCell node.ChildNodes[4]
+    let industryNode = extractValueFromScreenerCell node.ChildNodes[5]
+    let countryNode = extractValueFromScreenerCell node.ChildNodes[6]
+    let marketCapNode = extractValueFromScreenerCell node.ChildNodes[7]
+    let priceNode = extractValueFromScreenerCell node.ChildNodes[9]
+    let changeNode = extractValueFromScreenerCell node.ChildNodes[10]
+    let volumeNode = extractValueFromScreenerCell node.ChildNodes[11]
+    {
+        ticker=tickerNode;
+        company=companyNode;
+        sector=sectorNode; 
+        industry=industryNode;
+        country=countryNode; 
+        marketCap=marketCapNode;
+        price=priceNode;
+        change=changeNode;
+        volume=volumeNode
+    }
+
+let tickers =
+    nodes 
+    |> skipAndTake 3 1                      // skip three tr nodes, take one that has the tickers table
+    |> Seq.collect (fun n -> n.ChildNodes)  // this should contain #text, <td>, #text
+    |> skipAndTake 1 1                      // skip one #text, take one that has td
+    |> Seq.collect (fun n -> n.ChildNodes)  // this should contain #text <table> #text
+    |> skipAndTake 1 1                      // skip one #text, take table
+    |> Seq.collect (fun n -> n.ChildNodes)  // this should be all tr nodes
+    |> Seq.skip 2                           // skip what looks like text element and a header row
+    |> Seq.map processScreenerRow
+
+tickers |> Seq.iter (fun t -> Console.WriteLine(t.ticker))
+
---
+++ Types.fs
@@ -0,0 +1,13 @@
+namespace FinvizScraper
+
+type ScreenerResult = {
+    ticker:string;
+    company:string;
+    sector:string;
+    industry:string;
+    country:string;
+    marketCap:string;
+    price:string;
+    change:string;
+    volume:string;
+}
