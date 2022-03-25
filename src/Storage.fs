2d078a8bda67a5916a690ab7369de925d36e94e5
unknown <laimis@gmail.com>
2022-03-25 11:02:16 -0700

    reuse mappers

--- src/Storage.fs
+++ src/Storage.fs
@@ -9,6 +9,16 @@ module Storage =
     // TODO: move to config
     let cnnString = "Server=localhost;Port=5432;Database=finviz;User Id=finviz;Password=finviz;Include Error Detail=true"
 
+    let private stockMapper (reader:RowReader) =
+        {
+            id = reader.int "id";
+            ticker = reader.string "ticker";
+            company = reader.string "name";
+            sector = reader.string "sector";
+            industry = reader.string "industry";
+            country = reader.string "country";
+        }
+
     let getStockByTicker ticker =
         let sql = "SELECT id,ticker,name,sector,industry,country FROM stocks WHERE ticker = @ticker"
 
@@ -17,14 +27,7 @@ module Storage =
             |> Sql.connect
             |> Sql.query sql
             |> Sql.parameters ["@ticker", Sql.string ticker]
-            |> Sql.execute ( fun reader -> {
-                        id = reader.int "id";
-                        ticker = reader.string "ticker";
-                        company = reader.string "name";
-                        sector = reader.string "sector";
-                        industry = reader.string "industry";
-                        country = reader.string "country";
-                    })
+            |> Sql.execute stockMapper
 
         match results with
             | [] -> None
@@ -35,10 +38,9 @@ module Storage =
     let saveStock (ticker:string) name sector industry country =
         
         let sql = @"INSERT INTO stocks (ticker,name,sector,industry,country)
-            VALUES (@ticker,@name,@sector,@industry,@country) RETURNING id"
+            VALUES (@ticker,@name,@sector,@industry,@country) RETURNING *"
 
-        let id = 
-            cnnString
+        cnnString
             |> Sql.connect
             |> Sql.query sql
             |> Sql.parameters [
@@ -48,16 +50,7 @@ module Storage =
                     "@industry", Sql.string industry;
                     "@country", Sql.string country
                 ]
-            |> Sql.executeRow (fun reader -> reader.int "id")
-            
-        {
-            id = id;
-            ticker = ticker;
-            company = name;
-            sector = sector;
-            industry = industry;
-            country = country;
-        }
+            |> Sql.executeRow stockMapper
 
     let deleteStock (stock:Stock) =
         let sql = "DELETE FROM stocks WHERE id = @stockId"
@@ -68,22 +61,22 @@ module Storage =
         |> Sql.parameters ["@stockId", Sql.int stock.id]
         |> Sql.executeNonQuery
 
+    let private screenerMapper (reader:RowReader) =
+        {
+            id = reader.int "id";
+            name = reader.string "name";
+            url = reader.string "url";
+        }
+
     let saveScreener name url =
-        let id =
-            cnnString
+        cnnString
             |> Sql.connect
-            |> Sql.query "INSERT INTO screeners (name,url) VALUES (@name,@url) RETURNING id"
+            |> Sql.query "INSERT INTO screeners (name,url) VALUES (@name,@url) RETURNING *"
             |> Sql.parameters [
                 "@name", Sql.string name;
                 "@url", Sql.string url
             ]
-            |> Sql.executeRow (fun reader -> reader.int("id"))
-
-        {
-            id = id;
-            name = name;
-            url = url;
-        }
+            |> Sql.executeRow screenerMapper
 
     let getScreenerByName name = 
 
@@ -92,13 +85,7 @@ module Storage =
             |> Sql.connect
             |> Sql.query "SELECT id,name,url FROM screeners WHERE name = @name"
             |> Sql.parameters [ "@name", Sql.string name ]
-            |> Sql.execute (fun reader ->
-                {
-                    id = reader.int "id";
-                    name = reader.string "name";
-                    url = reader.string "url";
-                }
-            )
+            |> Sql.execute screenerMapper
         
         match results with
         | [] -> None
