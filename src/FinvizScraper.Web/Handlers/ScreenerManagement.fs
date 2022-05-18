06a991cd29a551825ba92a9b163d4db05d424a6f
unknown <laimis@gmail.com>
2022-05-18 16:16:51 -0700

    add screener page with table

--- src/FinvizScraper.Web/Handlers/ScreenerManagement.fs
+++ src/FinvizScraper.Web/Handlers/ScreenerManagement.fs
@@ -5,8 +5,41 @@ module ScreenerManagement =
     open Giraffe.ViewEngine
     open FinvizScraper.Web.Shared
     open FinvizScraper.Storage
-    open System
-
+    
     let handler() = 
         
-        Views.notFound "Implementation in progress"
+        let screeners = Storage.getScreeners()
+
+        let screenerRows =
+            screeners
+            |> List.map ( fun screener ->
+                tr [] [
+                    td [] [str (screener.id.ToString())]
+                    td [] [str screener.name]
+                    td [] [screener.url |> Views.generateHref "Finviz" ]
+                    td [] []
+                ]
+            )
+
+        let rows = [
+            thead [] [
+                tr [] [
+                    th [] [ str "Id" ]
+                    th [] [ str "Name" ]
+                    th [] [ str "Url" ]
+                    th [] [ str "Actions" ]
+                ]
+            ]
+            tbody [] screenerRows
+        ]
+        let screenerTable = rows |> Views.fullWidthTable
+
+        let content =
+            div [ _class "container" ] [
+                div [ _class "content" ] [
+                    h1 [ _class "title" ] [ str "Screener Management" ]
+                ]
+                screenerTable
+            ]
+
+        [content] |> Views.mainLayout "Screeners"
