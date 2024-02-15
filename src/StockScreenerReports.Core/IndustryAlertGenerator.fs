cd71a466303ea37f3e629611d8b3ec1ca998d9f7
Laimonas Simutis <laimis@gmail.com>
2024-02-15 10:42:14 -0800

    introduce screener industry alerts section

---
+++ src/StockScreenerReports.Core/IndustryAlertGenerator.fs
@@ -0,0 +1,74 @@
+module StockScreenerReports.Core.IndustryAlertGenerator
+
+[<Literal>]
+let private RESULT_COUNT_THRESHOLD = 20
+[<Literal>]
+let private PERCENT_CHECK_THRESHOLD = 0.5
+[<Literal>]
+let private INDUSTRY_PERCENT_CHECK_THRESHOLD = 0.4
+
+let generate industrySizeMap screenersWithResults =
+    
+    let countCheck industryName screener (results:ScreenerResultReportItem list) =
+        
+        match results |> List.length with
+        | x when x >= RESULT_COUNT_THRESHOLD ->
+            let alert = {
+                industry = industryName
+                alertType = "Count"
+                description = industryName + " has large participation, " + x.ToString() + " results"
+                screener = screener
+                date = results.Head.date
+            }
+            Some alert
+        | _ -> None
+        
+    let percentCheck industryName screener (industryResults:list<ScreenerResultReportItem>) totalResults =
+        
+        match (float industryResults.Length) / (float totalResults) with
+        | x when x >= PERCENT_CHECK_THRESHOLD ->
+            let alert = {
+                industry = industryName
+                alertType = "Percent"
+                description = $"{industryName} has " + x.ToString("P2") + $" percent of the results {industryResults.Length}/{totalResults}"
+                screener = screener
+                date = industryResults.Head.date
+            }
+            Some alert
+        | _ -> None
+        
+    let industryPercentCheck industryName screener (industryResults:ScreenerResultReportItem list) =
+        
+        let industryTotal = industrySizeMap |> Map.find industryName
+        
+        match (float industryResults.Length) / (float industryTotal) with
+        | x when x >= INDUSTRY_PERCENT_CHECK_THRESHOLD ->
+            let alert = {
+                industry = industryName
+                alertType = "IndustryPercent"
+                description = $"{industryName} has " + x.ToString("P2") + $" of the industry participants {industryResults.Length}/{industryTotal}"
+                screener = screener
+                date = industryResults.Head.date 
+            }
+            Some alert
+        | _ -> None
+    
+    // we will go over each screener,results pair in the screenersWithResults
+    // and generate an alert for each industry that has a result
+    screenersWithResults
+    |> List.map (fun (screener:Screener, results:ScreenerResultReportItem list) ->
+        
+        results
+        |> List.groupBy (fun (r:ScreenerResultReportItem) -> r.industry)
+        |> List.map (fun (industry, industryResults) ->
+            [
+                countCheck industry screener industryResults
+                percentCheck industry screener industryResults results.Length
+                industryPercentCheck industry screener industryResults
+            ]
+        )
+        |> List.concat
+        |> List.choose id
+    )
+    |> List.concat
+    
---
+++ tests/IndustryAlertGeneratorTests.fs
@@ -0,0 +1,120 @@
+module IndustryAlertGeneratorTests
+
+open System
+open StockScreenerReports.Core
+open Xunit
+open FsUnit
+
+
+type IndustryAlertGeneratorTests() =
+    let screener : Screener = {
+        id = 1
+        name = "name"
+        url = "url"
+    }
+    
+    let generateScreenerResultForIndustry industry : ScreenerResultReportItem =
+        {
+            country = "country"
+            industry = industry
+            price = 1.0m
+            sector = "sector"
+            ticker = "amd"
+            change = 10m
+            volume = 12
+            marketCap = 12m
+            date = DateTime.UtcNow
+            name = "companyname"
+            screenerid = 1
+            screenername = "screenername"
+            stockid = 1 
+        }
+        
+    let generateIndustrySMABreakdown count industry : IndustrySMABreakdown =
+        {
+            industry = industry
+            breakdown = {
+                above = 5
+                below = count
+                date = DateTime.UtcNow
+                days = SMA.SMA20 
+            } 
+        }
+        
+    let generateMapFromLists count (input:ScreenerResult list list) =
+        input
+        |> List.concat
+        |> List.distinctBy _.industry
+        |> List.map _.industry
+        |> List.distinct
+        |> List.map (fun x -> (x, count))
+        |> Map.ofList
+    
+    [<Fact>]
+    let ``When industry has more than 20 results, it ends up on alerts``() =
+        
+        let firstList =
+            [
+                yield! List.init 20 (fun _ -> generateScreenerResultForIndustry "industry1")
+                yield! List.init 5 (fun _ -> generateScreenerResultForIndustry "industry2")
+                yield! List.init 30 (fun _ -> generateScreenerResultForIndustry "industry3")
+            ]
+            
+        let secondList = List.init 20 (fun _ -> generateScreenerResultForIndustry "industry4")
+        
+        let screenerMap = [firstList; secondList] |> generateMapFromLists 25
+        
+        let input = [(screener,firstList); (screener,secondList)]
+        
+        let result = IndustryAlertGenerator.generate screenerMap input
+        
+        result |> should contain "industry1"
+        result |> should contain "industry3"
+        result |> should not' (contain "industry2")
+        result |> should contain "industry4"
+        
+    [<Fact>]
+    let ``When industry results are dominating percentage wise, it ends up on alerts``() =
+        
+        let firstList =
+            [
+                yield! List.init 10 (fun _ -> generateScreenerResultForIndustry "industry1")
+                yield! List.init 2 (fun _ -> generateScreenerResultForIndustry "industry2")
+                yield! List.init 2 (fun _ -> generateScreenerResultForIndustry "industry3")
+            ]
+            
+        let secondList =
+            [
+                yield! List.init 1 (fun _ -> generateScreenerResultForIndustry "industry4")
+                yield! List.init 1 (fun _ -> generateScreenerResultForIndustry "industry5")
+                yield! List.init 1 (fun _ -> generateScreenerResultForIndustry "industry6")
+                yield! List.init 1 (fun _ -> generateScreenerResultForIndustry "industry7")
+            ]
+        
+        let input = [(screener,firstList); (screener,secondList)]
+        
+        let screenerMap = [firstList; secondList] |> generateMapFromLists 25
+        
+        let result = IndustryAlertGenerator.generate screenerMap input
+        
+        result.Length |> should equal 1
+        result |> should contain "industry1"
+        
+    [<Fact>]
+    let ``When industry results are dominating industry breakdown, it ends up on alerts``() =
+        
+        let list = [
+            yield! List.init 5 (fun _ -> generateScreenerResultForIndustry "industry1")
+            yield! List.init 5 (fun _ -> generateScreenerResultForIndustry "industry2")
+            yield! List.init 5 (fun _ -> generateScreenerResultForIndustry "industry3")
+            yield! List.init 5 (fun _ -> generateScreenerResultForIndustry "industry4")
+        ]
+        
+        let screenerMap = [list] |> generateMapFromLists 25 |> Map.add "industry1" (generateIndustrySMABreakdown 5 "industry1")
+        
+        let input = [(screener,list)]
+        
+        let result = IndustryAlertGenerator.generate screenerMap input
+        
+        result.Length |> should equal 1
+        result |> should contain "industry1"
