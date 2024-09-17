1b2d72c3dbcb2cf6385b68ffcb7c236e44ed6a25
Laimonas Simutis <laimis@gmail.com>
2024-09-17 13:09:33 -0700

    simplify calling

--- src/StockScreenerReports.Web/Program.fs
+++ src/StockScreenerReports.Web/Program.fs
@@ -49,7 +49,7 @@ let configureJobs (app : IApplicationBuilder) =
         RecurringJob.AddOrUpdate<Services>(
             recurringJobId="fullrun",
             methodCall=(fun (s:Services) -> s.FullRun() :> Task),
-            cronExpression=Cron.Daily(16, 08),
+            cronExpression=Cron.Daily(16, 00),
             options=rjo
         )
         
